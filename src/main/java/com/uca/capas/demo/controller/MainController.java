39d18d2f0cf477da769981a11e15c86920b78f58
cortezle <00029117@uca.edu.sv>
2020-04-24 21:49:33 -0600

    agregar validacion de fecha

--- src/main/java/com/uca/capas/demo/controller/MainController.java
+++ src/main/java/com/uca/capas/demo/controller/MainController.java
@@ -1,7 +1,12 @@
 package com.uca.capas.demo.controller;
 
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -23,18 +28,18 @@ public class MainController {
 		ModelAndView mav = new ModelAndView();
 		String nombre = request.getParameter("nombre");
 		String apellido = request.getParameter("apellido");
+		String fechaN = request.getParameter("fechaN");
 		String lugar = request.getParameter("lugar");
 		String instituto = request.getParameter("instituto");
 		String tFijo = request.getParameter("tel");
 		String tMovil = request.getParameter("cel");
-		//ingresar la fecha con string con formato AAAA/MM/DD y hacerle split y trabajarlo con lista
-		String dia = request.getParameter("fechaN").toString();
-		
 		
 		List<String> listaErrores = new ArrayList<>();
 		
+		
 		validarNombre( nombre, listaErrores, mav);
 		validarApellido(apellido,listaErrores,mav);
+		validarFecha(fechaN, listaErrores, mav);
 		validarLugarDeNacimiento(lugar,listaErrores,mav);
 		validarInstituto(instituto,listaErrores,mav);
 		validarTelefonoFijo(tFijo,listaErrores,mav);
@@ -45,10 +50,8 @@ public class MainController {
 			mav.addObject("apellido", apellido);
 			mav.setViewName("registro_error.html");
 		}else {
-			mav.addObject("apellido", "exito");
 			mav.setViewName("registrado.html");
 		}
-		mav.addObject("fechaN", dia);
 		return mav;
 	}
 	
@@ -87,56 +90,56 @@ public class MainController {
 	public void validarNombre(String nombre, List<String> listaErrores, ModelAndView mav) {
 		if(validarMayorCero(nombre)) {
 			if(validarMayor25(nombre)) {
-				listaErrores.add("El nombre fue mayor a 25");
+				listaErrores.add("El nombre no puede ser mayor a 25 caracteres");
 			}
 			else {
 				mav.addObject("nombre", nombre);
 			}
 			
 		}else {
-			listaErrores.add("El nombre esta vacio");
+			listaErrores.add("El nombre no puede estar vacio");
 		}
 		
 	}
 	public void validarApellido(String apellido, List<String> listaErrores, ModelAndView mav) {
 		if(validarMayorCero(apellido)) {
 			if(validarMayor25(apellido)) {
-				listaErrores.add("El apellido fue mayor a 25");
+				listaErrores.add("El apellido no puede ser mayor a 25 caracteres");
 			}
 			else {
 				mav.addObject("apellido", apellido);
 			}
 			
 		}else {
-			listaErrores.add("El apellido esta vacio");
+			listaErrores.add("El apellido no puede estar vacio");
 		}
 	}
 	
 	public void validarLugarDeNacimiento(String lugar, List<String> listaErrores, ModelAndView mav) {
 		if(validarMayorCero(lugar)) {
 			if(validarMayor25(lugar)) {
-				listaErrores.add("El lugar de nacimiento fue mayor a 25");
+				listaErrores.add("El lugar de nacimiento no puede ser mayor a 25 caracteres");
 			}
 			else {
 				mav.addObject("lugar", lugar);
 			}
 			
 		}else {
-			listaErrores.add("El lugar de nacimiento esta vacio");
+			listaErrores.add("El lugar de nacimiento no puede estar vacio");
 		}
 	}
 	
 	public void validarInstituto(String instituto, List<String> listaErrores, ModelAndView mav) {
 		if(validarMayorCero(instituto)) {
 			if(validarMayor100(instituto)) {
-				listaErrores.add("El lugar de instituto fue mayor a 25");
+				listaErrores.add("El lugar de instituto no puede ser mayor a 100 caracteres");
 			}
 			else {
 				mav.addObject("instituto", instituto);
 			}
 			
 		}else {
-			listaErrores.add("El lugar de instituto esta vacio");
+			listaErrores.add("El lugar de instituto no puede estar vacio");
 		}
 	}
 	
@@ -144,7 +147,7 @@ public class MainController {
 		if(validarIgual8(tel)) {
 			mav.addObject("tel", tel);
 		}else {
-			listaErrores.add("El telefono tiene que ser de 8 numeros");
+			listaErrores.add("El telefono fijo tiene que ser de 8 numeros");
 		}
 		
 	}
@@ -153,9 +156,26 @@ public class MainController {
 		if(validarIgual8(cel)) {
 			mav.addObject("cel", cel);
 		}else {
-			listaErrores.add("El telefono Movil tiene que ser de 8 numeros");
+			listaErrores.add("El telefono movil tiene que ser de 8 numeros");
 		}
 		
+	}
+	public void validarFecha(String fechaN, List<String> listaErrores, ModelAndView mav) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		if(fechaN=="") {
+			fechaN="0001-01-01";
+		}
+		LocalDate fechaNac = LocalDate.parse(fechaN.toString(), formatter);
+		LocalDate fechaMin = LocalDate.parse("2003-01-01", formatter);
+
+		if(fechaNac.isAfter(fechaMin)){
+		   //fecha buena
+			mav.addObject("fechaN", fechaN);
+			
+		}else{
+		   //fecha error
+			listaErrores.add("Fecha tiene que ser mayor a 2003-01-01");
+		}
 	}
 	
 
